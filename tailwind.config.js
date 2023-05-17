3fa1bb0f5de6df97eebdf53eb9d0a73170710659
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-05-17 16:30:44 -0300

    fazendo umas modificações no tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,8 @@ module.exports = {
       colors: {
         gray: {
           50: '#eaeaea',
-          100: ' #bebebf',
-          200: ' #9e9ea0',
+          100: '#bebebf',
+          200: '#9e9ea0',
           300: '#727275',
           400: '#56565a',
           500: '#2c2c31',
