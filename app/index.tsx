84df4575214abbbc3460fee55402978982c03067
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-05-19 22:44:35 -0300

    criando a página Home da aplicação

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,10 @@
-import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from '@expo-google-fonts/roboto'
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-import blurBg from '../src/assets/bg-blur.png'
-import Stripes from '../src/assets/stripe.svg'
+import { Text, TouchableOpacity, View } from 'react-native'
 import React, { useEffect } from 'react'
-import { styled } from 'nativewind'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 import { useAuthRequest, makeRedirectUri } from 'expo-auth-session'
 import * as SecureStore from 'expo-secure-store'
 import { api } from '../src/lib/api'
 import { useRouter } from 'expo-router'
-
-const StyledStripes = styled(Stripes)
 
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
@@ -27,11 +15,6 @@ const discovery = {
 
 export default function App() {
   const router = useRouter()
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
 
   const [, response, signInWithGithub] = useAuthRequest(
     {
@@ -64,17 +47,8 @@ export default function App() {
     }
   }, [response])
 
-  if (!hasLoadedFonts) {
-    return null
-  }
-
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{ position: 'absolute', left: '-100%' }}
-    >
-      <StyledStripes className="absolute left-2" />
+    <View className="relative flex-1 items-center bg-gray-900 px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
         <View className="space-y-2">
@@ -99,7 +73,6 @@ export default function App() {
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito com 💜 no NLW da Rocketseat
       </Text>
-      <StatusBar style="light" translucent />
-    </ImageBackground>
+    </View>
   )
 }
